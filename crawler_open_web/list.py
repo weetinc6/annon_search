0d6681f2963d251fbdaec103b74f8e430dadb0f9
Crock <73002434+weetinc6@users.noreply.github.com>
2024-04-17 16:55:09 -0700

    all files will be updated

---
+++ crawler_open_web/list.py
@@ -0,0 +1,28 @@
+class SiteList:
+    def __init__(self, site_link) -> any:
+        self.site_link = site_link
+        pass
+
+    def siteLink(self):
+        return self.site_link
+    
+
+
+
+
+list = []
+list.append('twitter')
+list.append('twitter')
+list.append('twitter')
+list.append('twitter')
+list.append('twitter')
+list.append('twitter')
+
+
+
+XLink = SiteList(list)
+print(XLink.siteLink())
+
+
+x = ('1', '2')
+print(x[0], end="\n")
